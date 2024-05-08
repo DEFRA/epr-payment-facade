cb1abe3c3323e9f071536d56ddf2ad76487ea235
Paul Shimmen <paul.shimmen@eviden.com>
2024-05-08 09:47:51 +0100

    Adding service collection extension for IOC and adding versioning extensions

---
+++ src/EPR.Payment.Facade/Extension/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using EPR.Payment.Facade.Services.Interfaces;
+using EPR.Payment.Facade.Services;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Payment.Facade.Extension
+{
+    [ExcludeFromCodeCoverage]
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+
+
+            return services;
+
+        }
+    }
+}
--- src/EPR.Payment.Facade/Program.cs
+++ src/EPR.Payment.Facade/Program.cs
@@ -1,3 +1,6 @@
+using Asp.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 var app = builder.Build();
 
