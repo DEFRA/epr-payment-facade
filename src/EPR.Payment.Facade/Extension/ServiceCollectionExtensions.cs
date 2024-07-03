7328ced15e2519c78d7a431cd9f7169393d9630a
Paul Shimmen <paul.shimmen@eviden.com>
2024-07-03 15:29:04 +0100

    Fixing dependencies. This commit has all unit tests working, all integration tests working, health checks working, and all other dependencies working so that controller actions are accessable

--- src/EPR.Payment.Facade/Extension/ServiceCollectionExtensions.cs
+++ src/EPR.Payment.Facade/Extension/ServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
-﻿using EPR.Payment.Facade.Common.RESTServices.Payments;
-using EPR.Payment.Facade.Common.RESTServices.Payments.Interfaces;
-using EPR.Payment.Facade.HealthCheck;
+﻿using EPR.Payment.Facade.HealthCheck;
 using EPR.Payment.Facade.Services.Payments.Interfaces;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Diagnostics.CodeAnalysis;
@@ -11,6 +9,7 @@ namespace EPR.Payment.Facade.Extension
     public static class ServiceCollectionExtensions
     {
         private static readonly string Ready = "ready";
+
         public static IServiceCollection AddDependencies(this IServiceCollection services)
         {
             // Register HTTP context accessor
@@ -19,15 +18,10 @@ namespace EPR.Payment.Facade.Extension
             // Register HTTP client factory
             services.AddHttpClient();
 
-            // Register payments
+            // Register payments services
             services.AddScoped<IPaymentsService, PaymentsService>();
-            services.AddScoped<IHttpGovPayService, HttpGovPayService>();
-            services.AddScoped<IHttpPaymentsService, HttpPaymentsService>();
-            services.AddHttpContextAccessor();
-            services.AddHttpClient();
 
             return services;
-
         }
 
         public static IServiceCollection AddServiceHealthChecks(this IServiceCollection services)
