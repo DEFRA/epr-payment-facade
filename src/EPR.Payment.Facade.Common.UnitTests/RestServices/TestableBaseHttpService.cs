45cf5468b3942f43ed68216c2ae9c7270d5e507e
Manish Agarwal <manish.agarwal.external@eviden.com>
2024-07-16 17:16:22 +0100

    Added BaseHttpService tests

---
+++ src/EPR.Payment.Facade.Common.UnitTests/RestServices/TestableBaseHttpService.cs
@@ -0,0 +1,24 @@
+﻿using EPR.Payment.Facade.Common.RESTServices;
+using Microsoft.AspNetCore.Http;
+
+namespace EPR.Payment.Facade.Common.UnitTests.RestServices
+{
+    public class TestableBaseHttpService : BaseHttpService
+    {
+        public TestableBaseHttpService(
+            IHttpContextAccessor httpContextAccessor,
+            IHttpClientFactory httpClientFactory,
+            string baseUrl,
+            string endPointName)
+            : base(httpContextAccessor, httpClientFactory, baseUrl, endPointName)
+        {
+        }
+
+        // Public wrappers for protected methods for testing purposes
+        public Task<T> PublicPost<T>(string url, object payload, CancellationToken cancellationToken) =>
+            Post<T>(url, payload, cancellationToken);
+
+        public void PublicSetBearerToken(string token) =>
+            SetBearerToken(token);
+    }
+}
