0c016c752a842d8ccfffc6d76fe3f3561e3274f8
Paul Shimmen <paul.shimmen@eviden.com>
2024-08-20 15:11:43 +0100

    Registration fees, request and response DTOs

---
+++ src/EPR.Payment.Facade.Common/Dtos/Response/RegistrationFees/RegistrationFeeResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace EPR.Payment.Facade.Common.Dtos.Response.RegistrationFees
+{
+    public class RegistrationFeeResponseDto
+    {
+        public decimal? BaseFee { get; set; }
+        public decimal? SubsidiariesFee { get; set; }
+        public decimal TotalFee { get; set; }
+        public List<FeeBreakdown> FeeBreakdowns { get; set; } = new List<FeeBreakdown>();
+    }
+
+    public class FeeBreakdown
+    {
+        public required string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
