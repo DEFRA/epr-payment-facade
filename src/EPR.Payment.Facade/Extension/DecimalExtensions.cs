a5f7bc5688cb4f54d94edc2790517265ac193a82
mw-forster <mark.forster@eviden.com>
2024-09-21 22:20:56 +0100

    re-named parameter

--- src/EPR.Payment.Facade/Extension/DecimalExtensions.cs
+++ src/EPR.Payment.Facade/Extension/DecimalExtensions.cs
@@ -2,26 +2,26 @@
 {
     public static class DecimalExtensions
     {
-        public static bool IsNullableDecimal(this decimal decimalProperty)
+        public static bool IsNullableDecimal(this decimal decimalToCheck)
         {
-            Type type = decimalProperty.GetType();
+            Type type = decimalToCheck.GetType();
 
             return Nullable.GetUnderlyingType(type) != null && Nullable.GetUnderlyingType(type) == typeof(decimal);
         }
 
-        public static bool IsNullableDecimal(this decimal? decimalProperty)
+        public static bool IsNullableDecimal(this decimal? decimalToCheck)
         {
-            Type type = decimalProperty.GetType();
+            Type type = decimalToCheck.GetType();
 
             return Nullable.GetUnderlyingType(type) != null && Nullable.GetUnderlyingType(type) == typeof(decimal);
         }
 
-        public static bool IsNullableDecimalSimple(this decimal decimalProperty)
+        public static bool IsNullableDecimalSimple(this decimal decimalToCheck)
         {
             return false;
         }
 
-        public static bool IsNullableDecimalSimple(this decimal? decimalProperty)
+        public static bool IsNullableDecimalSimple(this decimal? decimalToCheck)
         {
             return true;
         }
