73946bb1796bdfac32e769a92421137008fde7e7
Paul Shimmen <167417965+paulshimmen@users.noreply.github.com>
2024-10-28 16:28:44 +0000

    Feature/447703 compliance scheme resubmission (#77)
    * Feature/tasks/459021 controller and service changes (#76)
    
    * Splitting index file into 3 pages.
    
    * Changes for compliance scheme resubmission
    
    * Fixing Sonar warnings
    
    * Changes from PR review
    
    * Merge from main

---
+++ src/EPR.Payment.Facade.Common/Dtos/Response/ResubmissionFees/ComplianceScheme/ComplianceSchemeResubmissionFeeResult.cs
@@ -0,0 +1,10 @@
+﻿namespace EPR.Payment.Facade.Common.Dtos.Response.ResubmissionFees.ComplianceScheme
+{
+    public class ComplianceSchemeResubmissionFeeResult
+    {
+        public decimal TotalResubmissionFee { get; set; }
+        public decimal PreviousPayments { get; set; }
+        public decimal OutstandingPayment { get; set; }
+        public int MemberCount { get; set; }
+    }
+}
---
+++ src/EPR.Payment.Facade/Validations/ResubmissionFees/ComplianceScheme/ComplianceSchemeResubmissionFeeRequestDtoValidator.cs
@@ -0,0 +1,40 @@
+﻿using EPR.Payment.Facade.Common.Constants;
+using EPR.Payment.Facade.Common.Dtos.Request.ResubmissionFees.ComplianceScheme;
+using EPR.Payment.Facade.Validations.Common;
+using FluentValidation;
+
+namespace EPR.Payment.Facade.Validations.ResubmissionFees.ComplianceScheme
+{
+    public class ComplianceSchemeResubmissionFeeRequestDtoValidator : AbstractValidator<ComplianceSchemeResubmissionFeeRequestDto>
+    {
+        public ComplianceSchemeResubmissionFeeRequestDtoValidator()
+        {
+            RuleFor(x => x.Regulator)
+                .NotEmpty().WithMessage(ValidationMessages.RegulatorRequired)
+                .Must(RegulatorValidationHelper.IsValidRegulator).WithMessage(ValidationMessages.RegulatorInvalid);
+
+            RuleFor(x => x.ResubmissionDate)
+                .NotEmpty().WithMessage(ValidationMessages.ResubmissionDateRequired)
+                .Must(BeInUtc).WithMessage(ValidationMessages.ResubmissionDateMustBeUtc)
+                .Must(BeValidDate).WithMessage(ValidationMessages.ResubmissionDateDefaultInvalid)
+                .LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ValidationMessages.ResubmissionDateInvalid);
+
+            RuleFor(x => x.ReferenceNumber)
+                .NotEmpty().WithMessage(ValidationMessages.ReferenceNumberRequired);
+
+            RuleFor(x => x.MemberCount)
+                .GreaterThan(0).WithMessage(ValidationMessages.MemberCountGreaterThanZero);
+        }
+
+        private bool BeInUtc(DateTime dateTime)
+        {
+            return dateTime.Kind == DateTimeKind.Utc;
+        }
+
+        private bool BeValidDate(DateTime dateTime)
+        {
+            // Here we check if the date is valid and if it is in a reasonable range
+            return dateTime != DateTime.MinValue && dateTime != default(DateTime);
+        }
+    }
+}
