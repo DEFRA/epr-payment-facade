eb1bf4d10dd8eb5ae37275184798895082aff0d1
Paul Shimmen <167417965+paulshimmen@users.noreply.github.com>
2024-11-20 13:03:02 +0000

    Feature/tasks/477036 add auth in swagger (#99)
    * Adding auth and feature flag
    
    * Adding mechanism for swagger to get token

--- src/EPR.Payment.Facade/Program.cs
+++ src/EPR.Payment.Facade/Program.cs
@@ -15,6 +15,9 @@ using System.Security.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Azure AD B2C configuration
+var azureAdB2CConfig = builder.Configuration.GetSection("AzureAdB2C");
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddFluentValidation(fv =>
@@ -32,15 +35,27 @@ builder.Services.AddSwaggerGen(setupAction =>
     setupAction.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentFacadeApi", Version = "v1" });
     setupAction.DocumentFilter<FeatureEnabledDocumentFilter>();
     setupAction.OperationFilter<FeatureGateOperationFilter>();
-    setupAction.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+
+    // Configure OAuth2 for Swagger
+    setupAction.AddSecurityDefinition("OAuth2", new OpenApiSecurityScheme
     {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter 'Bearer' followed by a space and your JWT token."
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri($"{azureAdB2CConfig["Instance"]}/{azureAdB2CConfig["Domain"]}/{azureAdB2CConfig["SignUpSignInPolicyId"]}/oauth2/v2.0/authorize"),
+                TokenUrl = new Uri($"{azureAdB2CConfig["Instance"]}/{azureAdB2CConfig["Domain"]}/{azureAdB2CConfig["SignUpSignInPolicyId"]}/oauth2/v2.0/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    { azureAdB2CConfig["Scopes"], "Access the Payment API" }
+                }
+            }
+        },
+        Description = "Authenticate with Azure AD B2C to access this API."
     });
+
+    // Require authentication for all endpoints in Swagger
     setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
@@ -49,10 +64,10 @@ builder.Services.AddSwaggerGen(setupAction =>
                 Reference = new OpenApiReference
                 {
                     Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
+                    Id = "OAuth2"
                 }
             },
-            Array.Empty<string>()
+            new[] { azureAdB2CConfig["Scopes"] }
         }
     });
 });
@@ -102,37 +117,22 @@ builder.Services.AddApiVersioning(options =>
 builder.Services.AddFeatureManagement();
 builder.Services.AddLogging();
 
-// Conditional Authentication based on Feature Flag
-using var serviceProvider = builder.Services.BuildServiceProvider();
-var featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
-if (await featureManager.IsEnabledAsync("EnableAuthenticationFeature"))
-{
-    // Authentication
-    builder.Services
-        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddMicrosoftIdentityWebApi(
-            options =>
-            {
-                builder.Configuration.Bind(Constants.AzureAdB2C, options);
-            },
-            options =>
-            {
-                builder.Configuration.Bind(Constants.AzureAdB2C, options);
-            });
-
-    // Authorization - Enforce authentication for all requests
-    builder.Services.AddAuthorization(options =>
-    {
-        options.FallbackPolicy = new AuthorizationPolicyBuilder()
-            .RequireAuthenticatedUser()
-            .Build();
-    });
-}
-
 var app = builder.Build();
 
+var featureManager = app.Services.GetRequiredService<IFeatureManager>();
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
 bool enableOnlinePaymentsFeature = await featureManager.IsEnabledAsync("EnableOnlinePaymentsFeature");
+bool enablePaymentInitiation = await featureManager.IsEnabledAsync("EnablePaymentInitiation");
+bool enablePaymentStatus = await featureManager.IsEnabledAsync("EnablePaymentStatus");
+bool enablePaymentStatusInsert = await featureManager.IsEnabledAsync("EnablePaymentStatusInsert");
 bool enableHomePage = await featureManager.IsEnabledAsync("EnableHomePage");
+
+logger.LogInformation("EnableOnlinePaymentsFeature: {EnableOnlinePaymentsFeature}", enableOnlinePaymentsFeature);
+logger.LogInformation("EnablePaymentInitiation: {EnablePaymentInitiation}", enablePaymentInitiation);
+logger.LogInformation("EnablePaymentStatus: {EnablePaymentStatus}", enablePaymentStatus);
+logger.LogInformation("EnablePaymentStatusInsert: {EnablePaymentStatusInsert}", enablePaymentStatusInsert);
+logger.LogInformation("EnableHomePage: {EnableHomePage}", enableHomePage);
 
 if (app.Environment.IsDevelopment())
 {
@@ -142,6 +142,12 @@ if (app.Environment.IsDevelopment())
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentFacadeApi v1");
         c.RoutePrefix = "swagger";
+
+        // OAuth2 settings for Swagger UI
+        c.OAuthClientId(azureAdB2CConfig["ClientId"]);
+        c.OAuthUsePkce(); // Use PKCE for secure authentication
+        c.OAuthScopeSeparator(" ");
+        c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
     });
 }
 
