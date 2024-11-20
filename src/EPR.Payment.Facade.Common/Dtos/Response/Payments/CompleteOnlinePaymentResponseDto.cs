269242b7e00b5d2629bbea5829d6dd78280db4a3
David Jayakumar <david.2.jayakumar.external@eviden.com>
2024-11-20 10:19:54 +0000

    feature/tasks/474350-Facade-Retrieve-description-and-pass-to-portal (#95)
    * feature/tasks/472383-Facade-Add-description-and-validation
    
    * Fixed review comments
    
    * pass Description to portal
    
    * Revert "pass Description to portal"
    
    This reverts commit c720f528ff7e91521404b1d56f8329d14dec27a2.
    
    * Reapply "pass Description to portal"
    
    This reverts commit 2f99c5ac50433f110bdc93b2b4ee02ac752c07f0.

--- src/EPR.Payment.Facade.Common/Dtos/Response/Payments/CompleteOnlinePaymentResponseDto.cs
+++ src/EPR.Payment.Facade.Common/Dtos/Response/Payments/CompleteOnlinePaymentResponseDto.cs
@@ -12,5 +12,6 @@ namespace EPR.Payment.Facade.Common.Dtos.Response.Payments
         public string? Regulator { get; set; }
         public decimal Amount { get; set; } = 0;
         public string? Email { get; set; }
+        public string? Description { get; set; }
     }
 }
