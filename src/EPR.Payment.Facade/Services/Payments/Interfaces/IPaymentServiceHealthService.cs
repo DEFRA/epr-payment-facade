7dd5b45ecb5470c56e0c6698487b6386f70a85be
Manish Agarwal <167305073+manishagarwalEviden@users.noreply.github.com>
2025-01-30 15:48:13 +0000

    AD501339 Removed custom health checks (#116)

--- src/EPR.Payment.Facade/Services/Payments/Interfaces/IPaymentServiceHealthService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EPR.Payment.Facade.Services.Payments.Interfaces
-{
-    public interface IPaymentServiceHealthService
-    {
-        Task<HttpResponseMessage> GetHealthAsync(CancellationToken cancellationToken);
-    }
-}
