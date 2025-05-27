6f36e32a5e5a5304cbeeb43630f88f393ae6cf9e
mahmood mahmood (Eviden) <mahmood.mahmood@Defra.onmicrosoft.com>
2025-05-27 11:25:32 +0100

    Reprocessor-Exporter Accreditation fees

---
+++ src/EPR.Payment.Facade.Common/Dtos/Response/AccreditationFees/AccreditationFeesPreviousPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Payment.Facade.Common.Dtos.Response.AccreditationFees
+{
+    public class AccreditationFeesPreviousPayment
+    {
+        public string? PaymentMode { get; set; } // "offline" or "online"
+
+        public string? PaymentMethod { get; set; }
+
+        public decimal PaymentAmount { get; set; }
+
+        public DateTime? PaymentDate { get; set; }
+    }
+}
