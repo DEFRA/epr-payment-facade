b18fb01b3e0bee925a113437261a9865514c3a70
Daniel Ashton <daniel.ashton@dashkat.uk>
2026-03-22 22:22:59 +0000

    AMCR-65 - Pass Fileid for getting submission fee details
    Move FileId to base request so that file id can be passed when getting submission payments, and also sent as part of offline payment request

--- src/EPR.Payment.Facade.Common/Dtos/Request/Payments/OfflinePaymentRequestDto.cs
+++ src/EPR.Payment.Facade.Common/Dtos/Request/Payments/OfflinePaymentRequestDto.cs
@@ -2,6 +2,8 @@
 {
     public class OfflinePaymentRequestDto : BasePaymentRequestDto
     {
+        public Guid? FileId { get; set; }
+
         public DateTime? PaymentDate { get; set; }
 
         public string? Comments { get; set; }
